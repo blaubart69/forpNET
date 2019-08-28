2811783fdd5de494daef298c83b5dbaead596e88
bee <bernhard.spindler.75@gmail.com>
2019-08-28 21:51:10 +0200

    moved out dryrun from RunOneProcess

--- forp.cs
+++ forp.cs
@@ -23,35 +23,34 @@ namespace forp
                             .Select(sub =>
                             {
                                 List<string> commandline = SubstitutePercent(commandTemplate, sub);
-                                return RunOneProcess(commandline, writer, opts.dryrun, cancel);
+                                string exe = commandline[0];
+                                string args = String.Join(" ", commandline.Skip(1));
+                                if (opts.dryrun)
+                                {
+                                    log.inf("[{0}] [{1}]", exe, args);
+                                    return Task.CompletedTask;
+                                }
+                                else
+                                {
+                                    return RunOneProcess(exe, args, writer, cancel);
+                                }
                             }),
                     MaxParallel: 2)
                 .Wait();
             }
         }
-        static Task RunOneProcess(List<string> commandline, TextWriter writer, bool dryrun, CancellationToken cancel)
+        static Task RunOneProcess(string exe, string args, TextWriter writer, CancellationToken cancel)
         {
-            string exe = commandline[0];
-            string args = String.Join(" ", commandline.Skip(1));
+            log.dbg("starting: [{0}] [{1}]", exe, args);
 
-            if (dryrun)
-            {
-                log.inf("[{0}] [{1}]", exe, args);
-                return Task.CompletedTask;
-            }
-            else
-            {
-                log.dbg("starting: [{0}] [{1}]", exe, args);
-
-                return
-                    ProcessRedirect.Start(
-                        new System.Diagnostics.ProcessStartInfo(exe, args),
-                        OnOutput: (kind, line) =>
-                        {
-                            writer.WriteLine(line);
-                        },
-                        cancel: cancel);
-            }
+            return
+                ProcessRedirect.Start(
+                    new System.Diagnostics.ProcessStartInfo(exe, args),
+                    OnOutput: (kind, line) =>
+                    {
+                        writer.WriteLine(line);
+                    },
+                    cancel: cancel);
         }
         static List<string> SubstitutePercent(List<string> commandTemplate, string[] substitutes)
         {
