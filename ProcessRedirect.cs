bb0f0308374e54f23e01ac78707ba6a6bc93c093
Bernhard Spindler <bernhard.spindler@unicredit.at>
2019-08-29 10:49:48 +0200

    exit when process could not be started

--- ProcessRedirect.cs
+++ ProcessRedirect.cs
@@ -17,53 +17,6 @@ namespace Spi
         static Log log = Log.GetLogger();
 
         public delegate void OutputHandler(KINDOFOUTPUT output, string line);
-
-        public static Task<int> Start(ProcessStartInfo pi, OutputHandler OnOutput, CancellationToken cancel)
-        {
-            Log logger = Log.GetLogger();
-
-            Process _proc = new Process()
-            {
-                StartInfo = pi,
-                EnableRaisingEvents = false
-            };
-            _proc.StartInfo.UseShellExecute = false;
-            _proc.StartInfo.RedirectStandardOutput = true;
-            _proc.StartInfo.RedirectStandardError = true;
-
-            _proc.OutputDataReceived += (object sender, DataReceivedEventArgs e) => { if (e.Data != null) { OnOutput(KINDOFOUTPUT.STDOUT, e.Data); } };
-            _proc.ErrorDataReceived  += (object sender, DataReceivedEventArgs e) => { if (e.Data != null) { OnOutput(KINDOFOUTPUT.STDERR, e.Data); } };
-
-            /*
-            Return Value
-            Type: System.Boolean
-            true if a process resource is started; 
-            false if no new process resource is started (for example, if an existing process is reused).
-            */
-
-            bool started = _proc.Start();
-            if (started == false)
-            {
-                logger.dbg("_proc.Start() returned false. no new process resource is started (for example, if an existing process is reused)");
-            }
-
-            _proc.BeginOutputReadLine();
-            _proc.BeginErrorReadLine();
-
-            return Task.Run(() =>
-            {
-                try
-                {
-                    _proc.WaitForExit();
-                    int RetCode = _proc.ExitCode;
-                    return RetCode;
-                }
-                finally
-                {
-                    _proc.Dispose();
-                }
-            });
-        }
         public static async Task<int> StartAsync(ProcessStartInfo pi, OutputHandler OnOutput, CancellationToken cancel)
         {
             Log logger = Log.GetLogger();
@@ -80,6 +33,7 @@ namespace Spi
                 catch (Win32Exception wex)
                 {
                     log.win32err(wex, "Process.Start()");
+                    return -1;
                 }
 
                 using (cancel.Register(() =>
@@ -113,6 +67,53 @@ namespace Spi
                 onLine(line);
             }
         }
+        /*
+public static Task<int> Start(ProcessStartInfo pi, OutputHandler OnOutput, CancellationToken cancel)
+{
+    Log logger = Log.GetLogger();
 
+    Process _proc = new Process()
+    {
+        StartInfo = pi,
+        EnableRaisingEvents = false
+    };
+    _proc.StartInfo.UseShellExecute = false;
+    _proc.StartInfo.RedirectStandardOutput = true;
+    _proc.StartInfo.RedirectStandardError = true;
+
+    _proc.OutputDataReceived += (object sender, DataReceivedEventArgs e) => { if (e.Data != null) { OnOutput(KINDOFOUTPUT.STDOUT, e.Data); } };
+    _proc.ErrorDataReceived  += (object sender, DataReceivedEventArgs e) => { if (e.Data != null) { OnOutput(KINDOFOUTPUT.STDERR, e.Data); } };
+
+
+    // Return Value
+    // Type: System.Boolean
+    // true if a process resource is started; 
+    // false if no new process resource is started (for example, if an existing process is reused).
+
+
+    bool started = _proc.Start();
+    if (started == false)
+    {
+        logger.dbg("_proc.Start() returned false. no new process resource is started (for example, if an existing process is reused)");
+    }
+
+    _proc.BeginOutputReadLine();
+    _proc.BeginErrorReadLine();
+
+    return Task.Run(() =>
+    {
+        try
+        {
+            _proc.WaitForExit();
+            int RetCode = _proc.ExitCode;
+            return RetCode;
+        }
+        finally
+        {
+            _proc.Dispose();
+        }
+    });
+}
+*/
     }
 }
