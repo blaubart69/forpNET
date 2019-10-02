d1f8fb0234231cc917e260b0c163f8564a7734a6
Bernhard Spindler <bernhard.spindler@unicredit.at>
2019-10-02 14:36:51 +0200

    little changes in the manual

--- forp/ProcessRedirectAsync.cs
+++ forp/ProcessRedirectAsync.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
